c101a10d32f8f5921e39525f4d1e7e87863b61d5
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-05-09 22:18:40 +0800

    update protobuf build script

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
-fn main() {
-    // compile protocol buffer using protoc
-    protoc_rust_grpc::Codegen::new()
-    .out_dir("src")
-    .input("./proto/hello.proto")
-    .rust_protobuf(true)
-    .run()
-    .expect("error compiling protocol buffer");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("proto/helloworld.proto")?;
+    Ok(())
 }
+
