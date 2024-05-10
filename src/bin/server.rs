d7b58b10cd0c977a6caffcfacce495cd4b1851a0
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-05-11 02:49:34 +0800

    remove unused code

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -87,59 +87,6 @@ async fn watch_file_write<P: AsRef<std::path::Path>>(path: P) -> notify::Result<
 
         if let Some(_event) = res.ok().filter(|event| event.kind == EventKind::Access(AccessKind::Close(notify::event::AccessMode::Write))) {
             println!("Some file done writing!");
-
-            // let files_to_process: Vec<PathBuf> = event.paths.into_iter() 
-            //     .filter(|f| {
-            //         f.is_file() 
-            //         && f.file_name().and_then(OsStr::to_str).is_some_and(|n| !n.starts_with("done"))
-            //         && f.extension().is_some_and(|n| n == "parquet")
-            //     }).collect();
-
-            // log::info!("{:?}",files_to_process);
-            // for filepath in files_to_process.into_iter()
-            //     {
-            //         let semaphore_clone = semaphore.clone();
-            //         let db_pool_clone = db_pool.clone();
-
-            //         let _ = tokio::spawn(async move {
-            //             log::info!("Waiting for permit to read newly created file");
-            //             let permit = semaphore_clone.acquire().await.unwrap();
-            //             log::info!("Successfully acquire permit to read newly created file");
-
-            //             let program_start_time = Instant::now();
-            //             match read_defer_record_from_parquet(filepath.clone()) {
-
-            //                 Ok(defer_records) => {
-
-            //                     if let Err(e) = insert_defer_record_into_database(defer_records, &db_pool_clone).await {
-
-            //                         log::error!("{:?}", e.to_string());
-
-            //                     } else {
-
-            //                         log::info!("Done inserting file {:?}", filepath);
-            //                         let name = filepath.file_name().unwrap();
-            //                         let mut new_name = filepath.clone();
-            //                         new_name.set_file_name(format!("done_{}", name.to_str().unwrap()));
-            //                         if let Some(ext) = filepath.extension() {
-
-            //                             new_name.set_extension(ext);
-            //                         }
-
-            //                         if let Err(e) = rename(filepath, new_name) {
-            //                             log::error!("Error occured when renaming file! {}", e.to_string());
-            //                         }
-            //                     }
-            //                 },
-            //                 Err(err) => {
-            //                     log::error!("Some Error occur while reading from parquet file: {}", err.to_string());
-            //                 }
-            //             };
-            //             log::info!("raw data import finished in {:.2?}, waiting for new task", program_start_time.elapsed());
-            //             drop(permit);
-            //         }).await;
-
-            //     }
         }
 
     }
@@ -147,7 +94,7 @@ async fn watch_file_write<P: AsRef<std::path::Path>>(path: P) -> notify::Result<
     Ok(())
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let greeter = MyGreeter::default();
